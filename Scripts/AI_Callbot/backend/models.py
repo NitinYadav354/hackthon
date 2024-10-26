04244948c960b53fcf5f53580bb125a122dce44e
NitinYadav354 <nyadav4625@gmail.com>
2024-10-26 07:06:44 +0530

    proceed

--- Scripts/AI_Callbot/backend/models.py
+++ Scripts/AI_Callbot/backend/models.py
@@ -4,7 +4,7 @@ from django.db import models
 class patient(models.Model):
     name = models.CharField(max_length=100)
     phone = models.CharField(max_length=15)
-    age = models.IntegerField((""))
+    age = models.IntegerField()
     symptoms = models.TextField()
     urgency = models.BooleanField(default=False)
 
@@ -16,19 +16,23 @@ class Appointment(models.Model):
     date = models.DateTimeField()
     doctor = models.CharField(max_length=100)
 
+
+class Department(models.Model):
+    Dept_name = models.CharField(max_length=50, unique=True)
+    Dept_D_no = models.IntegerField()
+    Dept_head = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.Dept_name
+    
+
 class Doctor(models.Model):
     Reg_no = models.CharField( max_length=50)
     D_name = models.CharField(max_length=50)
     D_phone = models.CharField(max_length=50)
-    D_Department = models.ForeignKey(patient, on_delete=models.CASCADE)
+    D_Department = models.ForeignKey(Department, on_delete=models.CASCADE)
     D_qualification = models.CharField(max_length=50)
     D_specialisation = models.CharField(max_length=50)
 
     def __str__(self):
         return self.D_name
-
-class Department(models.Model):
-    Dept_name = models.CharField(max_length=50)
-    Dept_D_no = models.IntegerField()
-    Dept_head = models.CharField(max_length=50)
-    
--- Scripts/AI_Callbot/backend/views.py
+++ Scripts/AI_Callbot/backend/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Appointment, Department, Doctor, patient
 
-# Create your views here.
+def home(request):
+    patient_count = patient.objects.count()
+    app_count = Appointment.objects.count()
+    doctor = Doctor.objects.all()
+    app = Appointment.objects.all()
+    return render(request, 'backend/index.html', {'patient_count':patient_count, 'app_count':app_count, 'doctor': doctor, 'app':app})
